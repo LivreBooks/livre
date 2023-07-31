accf256802d0dc3b606cb9b95be82939709a1eba
Patrick-web <pntxall100@gmail.com>
2023-07-31 22:59:49 +0300

    Check if file exists

--- components/viewer/PdfViewer.tsx
+++ components/viewer/PdfViewer.tsx
@@ -5,86 +5,94 @@ import Pdf from "react-native-pdf";
 
 import { theme } from "../../constants";
 import ViewerLoading from "./ViewerLoading";
+import { ALERT_TYPE, Toast } from "react-native-alert-notification";
 
 const PdfViewer = React.forwardRef(
-  (
-    {
-      fileUri,
-      bookCover,
-      page,
-      setPages,
-      setCurrentpage,
-    }: {
-      fileUri: string;
-      bookCover: string;
-      page?: number;
-      setPages: (value: number) => void;
-      setCurrentpage: (value: number) => void;
-    },
-    ref
-  ) => {
-    const pdfViewerRef = useRef<Pdf>(null);
+	(
+		{
+			fileUri,
+			bookCover,
+			page,
+			setPages,
+			setCurrentpage,
+		}: {
+			fileUri: string;
+			bookCover: string;
+			page?: number;
+			setPages: (value: number) => void;
+			setCurrentpage: (value: number) => void;
+		},
+		ref
+	) => {
+		const pdfViewerRef = useRef<Pdf>(null);
 
-    useImperativeHandle(ref, () => ({
-      jumpToPage(page: number) {
-        pdfViewerRef.current?.setPage(page);
-      },
-    }));
+		useImperativeHandle(ref, () => ({
+			jumpToPage(page: number) {
+				pdfViewerRef.current?.setPage(page);
+			},
+		}));
 
-    useEffect(() => {
-      //console.log({ page });
-      FileSystem.getInfoAsync(fileUri)
-        .then((fileinfo) => {
-          //console.log(fileinfo);
-        })
-        .catch((error) => {
-          //console.log(error);
-        });
-    }, []);
+		useEffect(() => {
+			FileSystem.getInfoAsync(fileUri)
+				.then((fileinfo) => {
+					if (fileinfo.exists === false) {
+						Toast.show({ title: "File Not Found", type: ALERT_TYPE.DANGER });
+					} else {
+						console.log("File exists");
+					}
+				})
+				.catch((error) => {
+					Toast.show({
+						title: "Error Checking If File Exists",
+						textBody: error?.message || "",
+						type: ALERT_TYPE.DANGER,
+					});
+				});
+		}, []);
 
-    return (
-      <View style={{ flex: 1 }}>
-        <Pdf
-          ref={pdfViewerRef}
-          renderActivityIndicator={() => (
-            <ViewerLoading bookCover={bookCover} />
-          )}
-          source={{ uri: `${decodeURIComponent(fileUri)}` }}
-          enableAnnotationRendering
-          onLoadComplete={(numberOfPages, filePath) => {
-            setPages(numberOfPages);
-          }}
-          onPageChanged={(page, numberOfPages) => {
-            setCurrentpage(page);
-          }}
-          onError={(error) => {
-            //console.log(error);
-          }}
-          onPressLink={(uri) => {
-            //console.log(`Link pressed: ${uri}`);
-          }}
-          page={page}
-          style={{
-            ...styles.pdf,
-          }}
-        />
-      </View>
-    );
-  }
+		return (
+			<View style={{ flex: 1 }}>
+				<Pdf
+					ref={pdfViewerRef}
+					renderActivityIndicator={() => (
+						<ViewerLoading bookCover={bookCover} />
+					)}
+					source={{ uri: `${decodeURIComponent(fileUri)}` }}
+					enableAnnotationRendering
+					onLoadComplete={(numberOfPages, filePath) => {
+						setPages(numberOfPages);
+					}}
+					onPageChanged={(page, numberOfPages) => {
+						setCurrentpage(page);
+					}}
+					onError={(error) => {
+						//console.log(error);
+					}}
+					onPressLink={(uri) => {
+						//console.log(`Link pressed: ${uri}`);
+					}}
+					page={page}
+					style={{
+						...styles.pdf,
+					}}
+				/>
+			</View>
+		);
+	}
 );
 
 export default PdfViewer;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get("window").width,
-    backgroundColor: theme.colors.background,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "flex-start",
+		alignItems: "center",
+		marginTop: 25,
+	},
+	pdf: {
+		flex: 1,
+		width: Dimensions.get("window").width,
+		backgroundColor: theme.colors.background,
+	},
 });
