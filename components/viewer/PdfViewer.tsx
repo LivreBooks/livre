4fe291f415c3e59376af1d58e03ea099b4e1e323
Just Patrick <56973177+Patrick-web@users.noreply.github.com>
2023-09-24 20:24:10 +0300

    Refactor: PdfViewer

--- components/viewer/PdfViewer.tsx
+++ components/viewer/PdfViewer.tsx
@@ -1,109 +1,92 @@
-import { Dimensions, StyleSheet, View } from "react-native";
 import * as FileSystem from "expo-file-system";
-import React, { useEffect, useRef, useImperativeHandle } from "react";
-import Pdf from "react-native-pdf";
-
-import { theme } from "../../constants";
-import ViewerLoading from "./ViewerLoading";
+import * as Linking from "expo-linking";
+import React, { useEffect, useImperativeHandle, useRef } from "react";
+import { Dimensions, View } from "react-native";
 import { ALERT_TYPE, Toast } from "react-native-alert-notification";
+import Pdf from "react-native-pdf";
+import { theme } from "../../constants";
 import { sentryCapture } from "../../utils";
+import ViewerLoading from "./ViewerLoading";
 
 export interface ExposedMethods {
 	jumpToPage: (page: number) => void;
 }
 
-const PdfViewer = React.forwardRef<ExposedMethods, {}>(
-	(
-		{
-			fileUri,
-			bookCover,
-			page,
-			setPages,
-			setCurrentpage,
-		}: {
-			fileUri: string;
-			bookCover: string;
-			page?: number;
-			setPages: (value: number) => void;
-			setCurrentpage: (value: number) => void;
+interface PdfProps {
+	fileUri: string;
+	bookCover: string;
+	page?: number;
+	setPages: (value: number) => void;
+	setCurrentPage: (value: number) => void;
+}
+
+const PdfViewer = React.forwardRef<ExposedMethods, PdfProps>(function PdfViewer(
+	{ fileUri, bookCover, page, setPages, setCurrentPage }: PdfProps,
+	ref
+) {
+	const pdfViewerRef = useRef<Pdf>(null);
+
+	useImperativeHandle(ref, () => ({
+		jumpToPage(page: number) {
+			pdfViewerRef.current?.setPage(page);
 		},
-		ref
-	) => {
-		const pdfViewerRef = useRef<Pdf>(null);
+	}));
 
-		useImperativeHandle(ref, () => ({
-			jumpToPage(page: number) {
-				pdfViewerRef.current?.setPage(page);
-			},
-		}));
+	useEffect(() => {
+		checkFileExists(fileUri);
+	}, []);
 
-		useEffect(() => {
-			FileSystem.getInfoAsync(fileUri)
-				.then((fileinfo) => {
-					console.log(fileinfo);
-					if (fileinfo.exists === false) {
-						Toast.show({ title: "File Not Found", type: ALERT_TYPE.DANGER });
-					} else {
-						console.log("File exists");
-					}
-				})
-				.catch((error) => {
-					sentryCapture(error);
+	return (
+		<View style={{ flex: 1 }}>
+			<Pdf
+				ref={pdfViewerRef}
+				renderActivityIndicator={() => <ViewerLoading bookCover={bookCover} />}
+				source={{ uri: `${decodeURIComponent(fileUri)}` }}
+				enableAnnotationRendering
+				onLoadComplete={(numberOfPages) => {
+					setPages(numberOfPages);
+				}}
+				onPageChanged={(page) => {
+					setCurrentPage(page);
+				}}
+				onError={(error: Error) => {
 					Toast.show({
-						title: "Error Checking If File Exists",
+						title: "Error Loading PDF",
 						textBody: error?.message || "",
-						type: ALERT_TYPE.DANGER,
 					});
-				});
-		}, []);
-
-		return (
-			<View style={{ flex: 1 }}>
-				<Pdf
-					ref={pdfViewerRef}
-					renderActivityIndicator={() => (
-						<ViewerLoading bookCover={bookCover} />
-					)}
-					source={{ uri: `${decodeURIComponent(fileUri)}` }}
-					enableAnnotationRendering
-					onLoadComplete={(numberOfPages, filePath) => {
-						setPages(numberOfPages);
-					}}
-					onPageChanged={(page, numberOfPages) => {
-						setCurrentpage(page);
-					}}
-					onError={(error) => {
-						Toast.show({
-							title: "Error Loading PDF",
-							textBody: error?.message || "",
-						});
-						sentryCapture(error as Error);
-					}}
-					onPressLink={(uri) => {
-						//console.log(`Link pressed: ${uri}`);
-					}}
-					page={page}
-					style={{
-						...styles.pdf,
-					}}
-				/>
-			</View>
-		);
-	}
-);
+					sentryCapture(error as Error);
+				}}
+				onPressLink={(uri) => {
+					Linking.openURL(uri);
+				}}
+				page={page}
+				style={{
+					backgroundColor: theme.colors.background,
+					flex: 1,
+					width: Dimensions.get("window").width,
+				}}
+			/>
+		</View>
+	);
+});
 
 export default PdfViewer;
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "flex-start",
-		alignItems: "center",
-		marginTop: 25,
-	},
-	pdf: {
-		flex: 1,
-		width: Dimensions.get("window").width,
-		backgroundColor: theme.colors.background,
-	},
-});
+function checkFileExists(fileUri: string) {
+	FileSystem.getInfoAsync(fileUri)
+		.then((fileInfo) => {
+			if (fileInfo.exists === false) {
+				Toast.show({ title: "File Not Found", type: ALERT_TYPE.DANGER });
+			} else {
+				console.log("File exists");
+			}
+		})
+		.catch((error) => {
+			sentryCapture(error);
+			Toast.show({
+				title: "Error Checking If File Exists",
+				textBody: error?.message || "",
+				type: ALERT_TYPE.DANGER,
+			});
+		});
+}
